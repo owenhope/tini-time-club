547798deb6ab6e0297f609fd40428b1c9229d80a
Owen Hope <owen@hopemediahouse.com>
2025-10-17 12:12:32 -0700

    [RELEASE] 2.0.6

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   return {
     ...config,
     name: name,
-    version: "2.0.5",
+    version: "2.0.6",
     slug: PROJECT_SLUG, // Must be consistent across all environments.
     orientation: "portrait",
     userInterfaceStyle: "automatic",
